04fe1c4bb3c1b48306fbe364413ff57d82da13e4
mithal <mithal1996@gmail.com>
2017-05-09 12:41:48 +0530

    added destroy feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-def index
+  def index
     @articles = Article.all
   end
  
@@ -8,33 +8,42 @@ def index
   end
  
   def new
-  @article = Article.new
-end
-def edit
-  @article = Article.find(params[:id])
-end
- 
-def create
-  @article = Article.new(article_params)
- 
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+    @article = Article.new
   end
-end
-def update
-  @article = Article.find(params[:id])
  
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+  def edit
+    @article = Article.find(params[:id])
   end
-end
  
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+ 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
